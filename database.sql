244084a4d86ad857cf771655a1d9efc7a2969c0a
William Noris <w.noris@protonmail.com>
2022-02-14 16:33:05 +0100

    Ajout de 2 tables et changement de clé sur genre

--- database.sql
+++ database.sql
@@ -1,23 +1,4 @@
--- MySQL dump 10.13  Distrib 8.0.27, for macos11 (x86_64)
---
--- Host: localhost    Database: bd-manga-shoten
--- ------------------------------------------------------
--- Server version	8.0.27
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `author`
---
 
 DROP TABLE IF EXISTS `author`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
@@ -73,9 +54,9 @@ DROP TABLE IF EXISTS `genre`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `genre` (
-  `id_type` varchar(255) NOT NULL,
+  `id_genre` varchar(255) NOT NULL,
   `name` varchar(50) NOT NULL,
-  PRIMARY KEY (`id_type`),
+  PRIMARY KEY (`id_genre`),
   UNIQUE KEY `name` (`name`)
 ) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -99,11 +80,11 @@ DROP TABLE IF EXISTS `genre_manga`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `genre_manga` (
   `id_manga` varchar(255) NOT NULL,
-  `id_type` varchar(255) NOT NULL,
-  PRIMARY KEY (`id_manga`,`id_type`),
-  KEY `id_type` (`id_type`),
+  `id_genre` varchar(255) NOT NULL,
+  PRIMARY KEY (`id_manga`,`id_genre`),
+  KEY `id_genre` (`id_genre`),
   CONSTRAINT `genre_manga_ibfk_1` FOREIGN KEY (`id_manga`) REFERENCES `manga` (`id_manga`),
-  CONSTRAINT `genre_manga_ibfk_2` FOREIGN KEY (`id_type`) REFERENCES `genre` (`id_type`)
+  CONSTRAINT `genre_manga_ibfk_2` FOREIGN KEY (`id_genre`) REFERENCES `genre` (`id_genre`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -116,6 +97,40 @@ LOCK TABLES `genre_manga` WRITE;
 INSERT INTO `genre_manga` VALUES ('31','1'),('32','2'),('33','3'),('34','4'),('35','5'),('36','6'),('37','7'),('38','8'),('39','9'),('40','10');
 /*!40000 ALTER TABLE `genre_manga` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `publication_demographic`
+--
+
+DROP TABLE IF EXISTS `publication_demographic`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `publication_demographic` (
+  `id` varchar(255) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+
+--
+-- Table structure for table `publication_demographic`
+--
+
+DROP TABLE IF EXISTS `publication_demographic_manga`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `publication_demographic_manga` (
+  `id_manga` varchar(255) NOT NULL,
+  `id_pub_demo` varchar(255) NOT NULL,
+  PRIMARY KEY (`id_manga`,`id_pub_demo`),
+  KEY `id_pub_demo` (`id_pub_demo`),
+  CONSTRAINT `pub_demo_manga_ibfk_1` FOREIGN KEY (`id_manga`) REFERENCES `manga` (`id_manga`),
+  CONSTRAINT `pub_demo_manga_ibfk_2` FOREIGN KEY (`id_pub_demo`) REFERENCES `publication_demographic` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
 
 --
 -- Table structure for table `manga`
@@ -272,14 +287,3 @@ LOCK TABLES `user_tome` WRITE;
 INSERT INTO `user_tome` VALUES (1,'3'),(2,'4'),(3,'5'),(4,'6'),(5,'7'),(6,'8'),(7,'9'),(8,'10'),(9,'11'),(10,'12');
 /*!40000 ALTER TABLE `user_tome` ENABLE KEYS */;
 UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2022-01-21 10:30:16
